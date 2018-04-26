eb640272b0d9cb9aa8a470b526ac3e8494e78f7e
Geoff Russell <Geoff.R.Russell@gmail.com>
2018-04-26 11:15:39 -0700

    Initial commit

---
+++ BTWB Date Helper.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         BTWB Date Helper
+// @namespace    https://russellgeoff.github.io/
+// @version      0.1
+// @description  Makes it easier to adjust the date on workouts
+// @author       You
+// @match        https://beyondthewhiteboard.com/workouts/logger
+// @match        https://beyondthewhiteboard.com/workouts/logger/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var run = function() {
+        if (location.hash.substr(0,8) != '#log?bto') return;
+
+        setTimeout(function () {
+        document.getElementById("workout_session_performedDate").type = "";
+        }, 500);
+    };
+
+    run();
+    window.onhashchange = run;
+})();
+
